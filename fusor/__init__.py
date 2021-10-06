52918b1f9c65129c65ccb23300094a89987db6c2
korikuzma <korikuzma@gmail.com>
2021-10-06 13:44:27 -0400

    Add logger

--- fusor/__init__.py
+++ fusor/__init__.py
@@ -1,6 +1,7 @@
 """Fusion package"""
 from pathlib import Path
 from os import environ
+import logging
 
 if "SEQREPO_DATA_PATH" in environ:
     SEQREPO_DATA_PATH = environ["SEQREPO_DATA_PATH"]
@@ -10,3 +11,9 @@ else:
 from fusor.fusor import FUSOR  # noqa: E402, F401
 
 APP_ROOT = Path(__file__).resolve().parents[0]
+
+logging.basicConfig(
+    filename="fusor.log",
+    format="[%(asctime)s] - %(name)s - %(levelname)s : %(message)s")
+logger = logging.getLogger("fusor")
+logger.setLevel(logging.DEBUG)
