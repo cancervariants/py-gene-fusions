6b59d4bc01da526a72064cb8d47963ee64acd2e7
korikuzma <korikuzma@gmail.com>
2021-10-05 20:52:44 -0400

    Set SEQREPO_DATA_PATH in init

--- fusor/__init__.py
+++ fusor/__init__.py
@@ -1,6 +1,14 @@
 """Fusion package"""
 from pathlib import Path
+from os import environ
 
-PROJECT_ROOT = Path(__file__).resolve().parents[0]
+APP_ROOT = Path(__file__).resolve().parents[0]
 
 __version__ = '0.0.9'
+
+if "SEQREPO_DATA_PATH" in environ:
+    SEQREPO_DATA_PATH = environ["SEQREPO_DATA_PATH"]
+else:
+    SEQREPO_DATA_PATH = f"{APP_ROOT}/data/seqrepo/latest"
+
+from fusor.fusor import FUSOR  # noqa: E402, F401
