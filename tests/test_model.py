e6ca8d8983a22b6786ba7d4f3b8d3103d23218ac
korikuzma <korikuzma@gmail.com>
2021-10-05 20:54:47 -0400

    WIP: Update tests

--- tests/test_model.py
+++ tests/test_model.py
@@ -2,12 +2,27 @@
 from pydantic import ValidationError
 import pytest
 import json
-from fusor import PROJECT_ROOT
+from fusor import APP_ROOT
 from fusor.models import TranscriptSegmentComponent, \
     GenomicRegionComponent, UnknownGeneComponent, GeneComponent, \
     AnyGeneComponent, LinkerComponent, CriticalDomain, Event, \
     RegulatoryElement, Fusion
+from fusor import FUSOR
 import copy
+
+EXAMPLES_DIR = APP_ROOT.resolve().parents[0] / 'examples'
+
+
+@pytest.fixture(scope='module')
+def fusor():
+    """Create test fixture for fusor object"""
+    return FUSOR()
+
+
+@pytest.fixture(scope='module')
+def fusion_example():
+    """Create test fixture for example of fusion"""
+    return json.load(open(EXAMPLES_DIR / 'exhaustive_example.json', 'r'))
 
 
 @pytest.fixture(scope='module')
@@ -626,11 +641,34 @@ def test_fusion(critical_domains, transcript_segments,
     check_validation_error(exc_info, msg)
 
 
-def test_examples():
-    """Test example JSON files."""
-    examples_dir = PROJECT_ROOT.resolve().parents[0] / 'examples'
-    example_big = json.load(open(examples_dir / 'exhaustive_example.json', 'r'))
-    assert Fusion(**example_big)
+def test_fusor(fusor, fusion_example):
+    """Test fusor methods."""
+    # TODO: Temporary tests for quickness
+    og_fusion = Fusion(**fusion_example)
+    copy_fusion = Fusion(**fusion_example)
 
-    example_1 = json.load(open(examples_dir / 'example_1.json', 'r'))
+    # Change sequence id
+    for transcript_component in og_fusion.transcript_components:
+        if isinstance(transcript_component, GenomicRegionComponent):
+            location = transcript_component.region.location
+            if location.type == "SequenceLocation":
+                transcript_component.region.location.sequence_id = "refseq:NC_000012.12"
+                fusor.set_sequence_id(og_fusion)
+                break
+    assert og_fusion == copy_fusion
+
+    # Change location id
+    for transcript_component in og_fusion.transcript_components:
+        if isinstance(transcript_component, GenomicRegionComponent):
+            transcript_component.region.location_id = None
+    fusor.set_location_id(og_fusion)
+
+    assert og_fusion == copy_fusion
+
+
+def test_examples(fusion_example):
+    """Test example JSON files."""
+    assert Fusion(**fusion_example)
+
+    example_1 = json.load(open(EXAMPLES_DIR / 'example_1.json', 'r'))
     assert Fusion(**example_1)
