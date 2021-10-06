d7d89183fb19aa90bf0ab51d19d7e48fb5e5dbdf
korikuzma <korikuzma@gmail.com>
2021-10-06 10:26:41 -0400

    WIP

--- fusor/__init__.py
+++ fusor/__init__.py
@@ -2,8 +2,6 @@
 from pathlib import Path
 from os import environ
 
-APP_ROOT = Path(__file__).resolve().parents[0]
-
 __version__ = "0.0.9"
 
 if "SEQREPO_DATA_PATH" in environ:
@@ -12,3 +10,5 @@ else:
     SEQREPO_DATA_PATH = "/usr/local/share/seqrepo/latest"
 
 from fusor.fusor import FUSOR  # noqa: E402, F401
+
+APP_ROOT = Path(__file__).resolve().parents[0]
