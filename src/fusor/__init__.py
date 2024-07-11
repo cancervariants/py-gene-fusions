837457957e8725ef742eaeb8aa6504037d075098
James Stevenson <james.stevenson@nationwidechildrens.org>
2024-07-11 09:02:45 -0400

    build!: use setuptools-scm for version declaration (#154)

--- src/fusor/__init__.py
+++ src/fusor/__init__.py
@@ -1,7 +1,15 @@
 """Provide computable object representation and validation for gene fusions"""
 
+from importlib.metadata import PackageNotFoundError, version
+
 from fusor.fusor import FUSOR
 
-from .version import __version__
-
 __all__ = ["__version__", "FUSOR"]
+
+
+try:
+    __version__ = version("cool_seq_tool")
+except PackageNotFoundError:
+    __version__ = "unknown"
+finally:
+    del version, PackageNotFoundError
