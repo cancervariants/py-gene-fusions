58b0899c41bcedf044acb0265ad97ddecb8fb5fb
Katie Stahl <kathryn.stahl@nationwidechildrens.org>
2024-07-22 13:41:27 -0400

    wip: start updates to nomenclature using new models

--- src/fusor/nomenclature.py
+++ src/fusor/nomenclature.py
@@ -1,6 +1,7 @@
 """Provide helper methods for fusion nomenclature generation."""
 
 from biocommons.seqrepo.seqrepo import SeqRepo
+from ga4gh.vrs.models import SequenceLocation
 
 from fusor.exceptions import IDTranslationException
 from fusor.models import (
@@ -23,18 +24,19 @@ def reg_element_nomenclature(element: RegulatoryElement, sr: SeqRepo) -> str:
         or if missing element reference ID, genomic location, and associated
         gene
     """
-    element_class = element.regulatory_class.value
+    element_class = element.regulatoryClass.value
     if element_class == RegulatoryClass.ENHANCER:
         type_string = "e"
     elif element_class == RegulatoryClass.PROMOTER:
         type_string = "p"
     else:
-        type_string = f"{element.regulatory_class.value}"
+        type_string = f"{element.regulatoryClass.value}"
     feature_string = ""
-    if element.feature_id:
-        feature_string += f"_{element.feature_id}"
-    elif element.feature_location:
-        start = element.feature_location
+    if element.featureId:
+        feature_string += f"_{element.featureId}"
+    elif element.featureLocation:
+        start = element.featureLocation
+        # TODO: update this with new model
         sequence_id = start.location.sequence_id
         refseq_id = str(translate_identifier(sr, sequence_id, "refseq")).split(":")[1]
         try:
@@ -42,17 +44,12 @@ def reg_element_nomenclature(element: RegulatoryElement, sr: SeqRepo) -> str:
         except IDTranslationException as e:
             raise ValueError from e
         feature_string += f"_{refseq_id}(chr {chrom}):g.{start.location.interval.start.value}_{start.location.interval.end.value}"
-    if element.associated_gene:
-        if element.associated_gene.gene_id:
-            gene_id = gene_id = element.associated_gene.gene_id
-
-        if element.associated_gene.gene_id:
-            gene_id = element.associated_gene.gene_id
-        elif element.associated_gene.gene and element.associated_gene.gene.gene_id:
-            gene_id = element.associated_gene.gene.gene_id
+    if element.associatedGene:
+        if element.associatedGene.id:
+            gene_id = element.associatedGene.id
         else:
             raise ValueError
-        feature_string += f"@{element.associated_gene.label}({gene_id})"
+        feature_string += f"@{element.associatedGene.label}({gene_id})"
     if not feature_string:
         raise ValueError
     return f"reg_{type_string}{feature_string}"
@@ -69,21 +66,21 @@ def tx_segment_nomenclature(element: TranscriptSegmentElement) -> str:
     if ":" in transcript:
         transcript = transcript.split(":")[1]
 
-    prefix = f"{transcript}({element.gene_descriptor.label})"
-    start = element.exon_start if element.exon_start else ""
-    if element.exon_start_offset:
-        if element.exon_start_offset > 0:
-            start_offset = f"+{element.exon_start_offset}"
+    prefix = f"{transcript}({element.gene.label})"
+    start = element.exonStart if element.exonStart else ""
+    if element.exonStartOffset:
+        if element.exonStartOffset > 0:
+            start_offset = f"+{element.exonStartOffset}"
         else:
-            start_offset = str(element.exon_start_offset)
+            start_offset = str(element.exonStartOffset)
     else:
         start_offset = ""
-    end = element.exon_end if element.exon_end else ""
-    if element.exon_end_offset:
-        if element.exon_end_offset > 0:
-            end_offset = f"+{element.exon_end_offset}"
+    end = element.exonEnd if element.exonEnd else ""
+    if element.exonEndOffset:
+        if element.exonEndOffset > 0:
+            end_offset = f"+{element.exonEndOffset}"
         else:
-            end_offset = str(element.exon_end_offset)
+            end_offset = str(element.exonEndOffset)
     else:
         end_offset = ""
     return f"{prefix}:e.{start}{start_offset}{'_' if start and end else ''}{end}{end_offset}"
@@ -93,6 +90,7 @@ def templated_seq_nomenclature(element: TemplatedSequenceElement, sr: SeqRepo) -
     """Return fusion nomenclature for templated sequence element.
 
     :param element: a templated sequence element
+    :param sr: SeqRepo instance to use
     :return: element nomenclature representation
     :raises ValueError: if location isn't a SequenceLocation or if unable
         to retrieve region or location
@@ -122,13 +120,8 @@ def gene_nomenclature(element: GeneElement) -> str:
     :return: element nomenclature representation
     :raises ValueError: if unable to retrieve gene ID
     """
-    if element.gene_descriptor.gene_id:
-        gene_id = gene_id = element.gene_descriptor.gene_id
-
-    if element.gene_descriptor.gene_id:
-        gene_id = element.gene_descriptor.gene_id
-    elif element.gene_descriptor.gene and element.gene_descriptor.gene.gene_id:
-        gene_id = element.gene_descriptor.gene.gene_id
+    if element.gene.id:
+        gene_id = element.gene.id
     else:
         raise ValueError
-    return f"{element.gene_descriptor.label}({gene_id})"
+    return f"{element.gene.label}({gene_id})"
