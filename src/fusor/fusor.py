8fd8d3fe602fb2ae1e56df3a0372b3188a8717c1
Katie Stahl <kathryn.stahl@nationwidechildrens.org>
2024-07-29 09:42:50 -0400

    Update src/fusor/fusor.py
    Co-authored-by: Kori Kuzma <korikuzma@gmail.com>

--- src/fusor/fusor.py
+++ src/fusor/fusor.py
@@ -569,8 +569,7 @@ class FUSOR:
         gene_norm_resp = self.gene_normalizer.normalize(query)
         if gene_norm_resp.match_type:
             gene = gene_norm_resp.gene
-            # remove normalize.gene from id (ex; 'normalize.gene.hgnc:1097' -> 'hgnc:1097')
-            gene_id = gene_norm_resp.normalized_id.replace("normalize.gene.", "")
+            gene_id = gene_norm_resp.normalized_id
             if use_minimal_gene:
                 return Gene(id=gene_id, label=gene.label), None
             gene.id = gene_id
